94400dbe666b5dc3e5a9adbf9c0cc5fcc32a6219
Mattheas <mattheas@ualberta.ca>
2019-12-01 19:17:29 -0700

    handshake code

--- encrypted_communication_part2.cpp
+++ encrypted_communication_part2.cpp
@@ -2,13 +2,35 @@
 #include <math.h>
 
 const int analogpin = 1;
+const int config_pin = 13;
+enum ConnectionState {
+  	START, WAITINGFORACK, DATAEXCHANGE, LISTEN, WAITINGFORKEY
+};
+
 
 void setup() {
 	init();
 	Serial.begin(9600);
 	pinMode(analogpin, INPUT);
-
 }
+
+
+bool display_arduino_type()
+{
+    bool server;
+
+    if (digitalRead(config_pin) == HIGH) {
+        bool server = true;
+        Serial.println("Server Arduino");
+    }
+    else {
+        bool server = false;
+        Serial.println("Client Arduino");
+    }
+    Serial.flush();
+    return server;
+}
+
 
 bool check_prime(uint32_t n) {
     bool check = true;
@@ -27,6 +49,7 @@ bool check_prime(uint32_t n) {
 
     return 0;
 }
+
 
 uint32_t generate_random_number(int n) {
 	int voltageReading;
@@ -73,8 +96,8 @@ uint32_t get_p_val(int p_k) {
 	}
 
 	return p_value;
-
 }
+
 
 uint32_t get_q_val(int q_k) {
 	uint32_t q_value;
@@ -90,14 +113,15 @@ uint32_t get_q_val(int q_k) {
 	}
 
 	return q_value;
-
 }
+
 
 uint32_t get_ard_mod(uint32_t p_value, uint32_t q_value) {
 	uint32_t mod = (p_value * q_value);
 
 	return mod;
 }
+
 
 uint32_t get_phi(uint32_t p_value, uint32_t q_value) {
 	uint32_t phi;
@@ -106,6 +130,7 @@ uint32_t get_phi(uint32_t p_value, uint32_t q_value) {
 
 	return phi;
 }
+
 
 unsigned int find_gcd(uint32_t pub_key, uint32_t phi)
 {	
@@ -120,6 +145,7 @@ unsigned int find_gcd(uint32_t pub_key, uint32_t phi)
     return pub_key;
 }
 
+
 uint32_t get_public_key(uint32_t phi_value) {
 	uint32_t pub_key = 0;
 	uint32_t n = 0;
@@ -130,8 +156,8 @@ uint32_t get_public_key(uint32_t phi_value) {
 	}
 
 	return pub_key;
-
 }
+
 
 uint32_t get_private_key(uint32_t pub_key, uint32_t phi_value, uint32_t myArd_mod) {
 	int32_t r[40], s[40], t[40];
@@ -175,18 +201,212 @@ uint32_t get_private_key(uint32_t pub_key, uint32_t phi_value, uint32_t myArd_mo
 
 } 
 
+
+
+
+
+
+
+
+
+
+
+
+uint32_t encrypt(uint32_t c, uint32_t e, uint32_t n)
+{
+    uint32_t ans = 0;
+    uint32_t pow_x = c;
+
+    // To prevent overflow we can use repeated multipication
+    while (e > 0) {
+        // e is odd then add pow_x to ans
+        if ((e % 2) == 1) {
+            ans = (ans + pow_x) % n;
+        }
+
+        // Multiply pow_x by 2 and then divide e by 2
+        pow_x = (pow_x * 2) % n;
+        e <<= 1;
+    }
+
+    return ans;
+}
+
+/* 
+  Description:
+  Takes in a character from server() or client() through serial3 and performs 
+  modular arthemtic to decrypt the character into a uint32_t variable 
+  and returns it back to server() or client()
+
+  Arguments:
+  uint32_t x = input byte from serial3
+  uint32_t d = Client's/Server's private key
+  uint32_t m = Client's/Server's modulus
+*/
+uint32_t decrypt(uint32_t x, uint32_t d, uint32_t m)
+{
+    uint32_t ans = 0;
+    uint32_t pow_x = x;
+
+    // To prevent overflow we can use repeated multipication
+    while (d > 0) {
+        // e is odd then add pow_x to ans
+        if ((d % 2) == 1) {
+            ans = (ans + pow_x) % m;
+        }
+
+        // Multiply pow_x by 2 and then divide e by 2
+        pow_x = (pow_x * 2) % m;
+        d <<= 1;
+    }
+    return ans;
+}
+
+/* 
+  Description:
+  Sends a byte at a time to serial3, due to the way characters
+  are encrypted in RSA
+
+  Arguments:
+  uint32_t num = encrypted character to be sent
+*/
+void uint32_to_serial3(uint32_t num)
+{
+	Serial3.flush();
+    Serial3.write((char)(num >> 0));
+    Serial3.write((char)(num >> 8));
+    Serial3.write((char)(num >> 16));
+    Serial3.write((char)(num >> 24));
+}
+
+/* 
+  Description:
+  Reads a byte at a time from serial3, due to the way characters
+  are encrypted in RSA
+*/
+uint32_t uint32_from_serial3()
+{
+    uint32_t num = 0;
+    num = num | ((uint32_t)Serial3.read()) << 0;
+    num = num | ((uint32_t)Serial3.read()) << 8;
+    num = num | ((uint32_t)Serial3.read()) << 16;
+    num = num | ((uint32_t)Serial3.read()) << 24;
+    // sends num to server() or client() functions
+    return num;
+}
+
+/* 
+  Description:
+  Declares the appropiate keys to the variables d, n, e and m.
+  Main purpose of thhis function is facilitate sending/recieving data
+  functions, encrypt/decrypt functions and write characters back onto
+  serial. The client() and server() function are analgous in their program
+  operation however, the main difference is the declaration of keys (public,
+  private and modulus)
+*/
+void server_arduino(uint32_t d, uint32_t n, uint32_t e, uint32_t m)
+{
+	Serial.println('hello we in server_arduino');
+    // if there is a byte to read, read it and send to Arduino B
+    //uint32_t d = ServerPrivateKey;
+    //uint32_t n = ServerModulus;
+    //uint32_t e = ClientPublicKey;
+    //uint32_t m = ClientModulus;
+    if (Serial.available() > 0) {
+        uint32_t byte_read = Serial.read(); // ASCII value of key pressed
+        Serial.write(byte_read);
+        Serial.flush();
+        uint32_t encrypt_ans = encrypt(byte_read, e, n);
+        uint32_to_serial3(encrypt_ans);
+
+        if (byte_read == 13) {
+            byte_read = 10;
+            Serial.write(byte_read);
+            Serial.flush();
+            uint32_t encrypt_ans = encrypt(byte_read, e, n);
+            uint32_to_serial3(encrypt_ans);
+        }
+    }
+    if (Serial3.available() > 3) {
+        uint32_t byte_read = uint32_from_serial3();
+        uint32_t decrypt_ans = decrypt(byte_read, d, m);
+        Serial.write(decrypt_ans);
+        Serial.flush();
+    }
+}
+
+/* 
+  Description:
+  Declares the appropiate keys to the variables d, n, e and m.
+  Main purpose of thhis function is facilitate sending/recieving data
+  functions, encrypt/decrypt functions and write characters back onto
+  serial. The client() and server() function are analgous in their program
+  operation however, the main difference is the declaration of keys (public,
+  private and modulus)
+*/
+void client_arduino(uint32_t d, uint32_t n, uint32_t e, uint32_t m)
+{
+    //uint32_t d = ClientPrivateKey;
+    //uint32_t n = ClientModulus;
+    //uint32_t e = ServerPublicKey;
+    //uint32_t m = ServerModulus;
+    if (Serial.available() > 0) {
+        uint32_t byte_read = Serial.read();
+        Serial.write(byte_read);
+        Serial.flush();
+        uint32_t encrypt_ans = encrypt(byte_read, e, n);
+        uint32_to_serial3(encrypt_ans);
+
+        if (byte_read == 13) {
+            byte_read = 10;
+            Serial.write(byte_read);
+            Serial.flush();
+            uint32_t encrypt_ans = encrypt(byte_read, e, n);
+            uint32_to_serial3(encrypt_ans);
+        }
+    }
+    if (Serial3.available() > 3) {
+        uint32_t byte_read = uint32_from_serial3();
+        uint32_t decrypt_ans = decrypt(byte_read, d, m);
+        Serial.write(decrypt_ans);
+        Serial.flush();
+    }
+}
+
+
+bool wait_on_serial3( uint8_t nbytes, long timeout){
+	unsigned long deadline = millis() + timeout;// wraparound not a problem
+	while(Serial3.available()<nbytes && (timeout<0 || millis()<deadline)){
+		delay(1);
+	}
+
+	return Serial3.available()>=nbytes;
+}
+
+
+
+
+
+
+
+
+
+
 int main() {
 	setup();
+	bool server;
+    server = display_arduino_type();// server = true then arudino is server else vice versa
 	uint32_t p_value = 0;
 	uint32_t q_value = 0;
 	uint32_t myArd_mod = 0;
 	uint32_t phi_value = 0;
 	uint32_t public_key = 0;
 	uint32_t private_key = 0;
+	uint32_t s_array[2]={0};
 	int p_k = 14;
 	int q_k = 15;
-
-	for (int i = 0; i < 30; i++) {
+	
+	//for (int i = 0; i < 30; i++) {
 	p_value = get_p_val(p_k);
 	//Serial.print("p value = ");
 	//Serial.println(p_value);
@@ -229,8 +449,100 @@ int main() {
 		Serial.println("public_key not domain");
 	}
 
-	//Serial.println(" ");
-	}
+	//} end of testing for loop
+	
+	
+
+
+	// code for server arduino
+	if(server==true){
+		// sets initial state
+		ConnectionState state = LISTEN;
+
+		while(state!=DATAEXCHANGE){
+			if(state==LISTEN){
+				if(Serial3.read()=="C"){
+					state=WAITINGFORKEY;
+				}
+			}else if(state==WAITINGFORKEY){
+				if( wait_on_serial3(8,1000) ){
+					//skey and smod are what we recive from other arduino
+					s_array[0] = uint32_from_serial3();
+					s_array[1] = uint32_from_serial3();
+
+					// acknowledge and send this arduinos public key/mod to other arduino
+					Serial3.print(65);
+					uint32_to_serial3(public_key);
+					uint32_to_serial3(myArd_mod);
+
+				}else{
+					//timeout return to LISTEN state
+					state=LISTEN;
+				}
+			}else if(state==WAITINGFORACK){
+				if(wait_on_serial3(1,1000)){
+					if(Serial3.read()=="A"){
+						state=DATAEXCHANGE;
+						
+					}else if(Serial3.read()=="C"){
+						state=WAITINGFORKEY;
+					}
+				}else{
+					state=LISTEN;
+				}
+			}
+		}// end of while loop
+
+	//code for client arduino
+	}else{
+		// sets inital state
+		ConnectionState state = START;
+
+		while(state!=DATAEXCHANGE){// state!=dataexchange
+			if(state == START){
+				// send connection request CR(ckey,mod) 9 bytes
+				Serial3.print(67);
+				uint32_to_serial3(public_key);
+				uint32_to_serial3(myArd_mod);
+
+				//change state to wait for acknowledge
+				state=WAITINGFORACK;
+
+			}else if(state== WAITINGFORACK){
+				if( wait_on_serial3(9,1000) ){
+					if(Serial3.read()=="A"){
+						// store skey, smod
+						s_array[0] = uint32_from_serial3();
+						s_array[1] = uint32_from_serial3();
+
+						// send acknowledge to server
+						Serial3.print(65);
+						state = DATAEXCHANGE;
+
+
+					}else{
+						state = START;
+					}	
+
+				}else{
+					// timeout and change state back to Start
+					state = START;
+				}
+			}
+		}// end of while loop	
+	}// end of else 
+
+
+	Serial.println("before infinte while loop");// not reaching here
+	//now run infinte while loop so arduinos can communicate
+	while (true) {
+        if (server == true) {
+            server_arduino(private_key, myArd_mod, s_array[0], s_array[1]);
+        }
+        else {
+            client_arduino(private_key, myArd_mod, s_array[0], s_array[1]);
+        }
+    }
 
 	return 0;
 }
